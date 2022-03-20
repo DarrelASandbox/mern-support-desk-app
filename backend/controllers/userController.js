f10526897533e6451cfe50efa8b6b77405ece1ee
DarrelA <darrelaiscoding@gmail.com>
2022-03-20 22:14:13 +0800

    loginUser(), userSchema.methods.generateAuthToken() with JWT, authMiddleware & getMe().

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
 const bcrypt = require('bcryptjs');
-const User = require('../models/userModels');
 
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
@@ -18,16 +18,45 @@ const registerUser = asyncHandler(async (req, res) => {
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
   const user = await User.create({ name, email, password: hashedPassword });
+  const token = await user.generateAuthToken(user._id);
 
   if (user)
-    return res
-      .status(201)
-      .json({ _id: user._id, name: user.name, email: user.email });
+    return res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token,
+    });
 
   res.status(400);
   throw new Error('Invalid user data.');
 });
 
-const loginUser = asyncHandler(async (req, res) => res.send('login'));
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  const token = await user.generateAuthToken(user._id);
 
-module.exports = { registerUser, loginUser };
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid credentials.');
+  }
+});
+
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+  };
+  res.status(200).json(user);
+});
+
+module.exports = { registerUser, loginUser, getMe };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 
-const userSchema = mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
@@ -20,10 +21,28 @@ const userSchema = mongoose.Schema(
       required: true,
       default: false,
     },
+    // array of objects
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
   },
   {
     timestamps: true,
   }
 );
+
+userSchema.methods.generateAuthToken = async function () {
+  const token = jwt.sign({ _id: this.id.toString() }, process.env.JWT_SECRET, {
+    expiresIn: '1d',
+  });
+  this.tokens = [...this.tokens, { token }];
+  await this.save();
+  return token;
+};
 
 module.exports = mongoose.model('User', userSchema);
